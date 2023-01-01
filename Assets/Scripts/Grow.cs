ecedc70fa7f3cb3e8ae1d144fd588ced29b7ac7a
berketurker <45368526+berkosko5000@users.noreply.github.com>
2023-01-01 14:31:30 +0300

    BerkePush

--- Assets/Scripts/Grow.cs
+++ Assets/Scripts/Grow.cs
@@ -7,28 +7,37 @@ public class Grow : MonoBehaviour
 
     [SerializeField] Transform clonesTab;
     public int count = 1;
-    private void OnCollisionEnter(Collision collision)
+    [SerializeField] GameObject _clone;
+
+    private void OnTriggerEnter(Collider other)
     {
-        int magnitude = collision.gameObject.GetComponent<Options>().optionMagnitude;
-        bool operatorIsMultiplication = collision.gameObject.GetComponent<Options>().isMultiplication;
-        Vector3 firstPos = gameObject.transform.position;
-        if (operatorIsMultiplication)
+
+        if (other.gameObject.GetComponent<Options>())
         {
-            Multiply(magnitude, firstPos, count);
+            int magnitude = other.gameObject.GetComponent<Options>().optionMagnitude;
+            bool operatorIsMultiplication = other.gameObject.GetComponent<Options>().isMultiplication;
+            Vector3 firstPos = gameObject.transform.position;
+            if (operatorIsMultiplication)
+            {
+                Multiply(magnitude, firstPos, count);
+            }
+
+            else
+            {
+                Add(magnitude, firstPos, count);
+            }
         }
 
-        else
-        {
-            Add(magnitude, firstPos, count);
-        }
     }
 
-    void Multiply(int magnitude, Vector3 firstPos,int count)
+
+    void Multiply(int magnitude, Vector3 firstPos, int count)
     {
         for (int i = 0; i < count * magnitude; i++)
         {
-            GameObject temp = Instantiate(gameObject, clonesTab);
-            temp.transform.position = firstPos;
+            GameObject temp = Instantiate(_clone, clonesTab);
+            Destroy(temp.GetComponent<Grow>());
+            temp.transform.position = new Vector3(firstPos.x+1,firstPos.y,firstPos.z-2);
         }
         count *= magnitude;
     }
@@ -37,7 +46,7 @@ public class Grow : MonoBehaviour
     {
         for (int i = 0; i < magnitude; i++)
         {
-            GameObject temp = Instantiate(gameObject, clonesTab);
+            GameObject temp = Instantiate(_clone, clonesTab);
             temp.transform.position = firstPos;
         }
         count += magnitude;

77172fb6c56c1d2f99c747bef0c65572d7d41b77
Muhammed Enes Ünal <menes_unal@hotmail.com>
2023-01-01 21:49:18 +0300

    animations and obstacles added

---
+++ Assets/Obstacle/BasicObstacleController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicObstacleController : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collidee)
+    {
+        if (collidee.gameObject.tag == "Ally")
+        {
+            collidee.GetComponent<PartyMember>().LeaveTeam();
+        }
+    }
+}

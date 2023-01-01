082dc488688302c9e21e7cf0fc085c6531257ffa
Baran İNCE <incebaran25@gmail.com>
2023-01-01 23:08:00 +0300

    Finalfinalfinal

--- Assets/Scripts/Grow/Grow.cs
+++ Assets/Scripts/Grow/Grow.cs
@@ -1,12 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Grow : MonoBehaviour
 {
     public int count = 1;
     [SerializeField] GameObject _clone;
-
+    [SerializeField] TMP_Text countText;
+    private void Awake()
+    {
+        countText = this.transform.GetChild(0).GetComponent< TMP_Text>();
+        int magnitude = this.gameObject.GetComponent<Options>().optionMagnitude;
+        ChannelType channelType = this.gameObject.GetComponent<Options>().channelType;
+        if (channelType == ChannelType.Multiplication)
+        {
+            count = count * magnitude;
+            countText.text = "x" + count.ToString();
+        }
+        else
+        {
+            count = count + magnitude;
+            countText.text = "+" + count.ToString();
+        }
+        
+        
+    }
     private void OnTriggerEnter(Collider other)
     {
         Channel channel = transform.parent.GetComponent<Channel>();
@@ -18,15 +37,6 @@ public class Grow : MonoBehaviour
             Vector3 firstPos = other.gameObject.transform.position;
             Transform parent = other.transform.parent;
             float randomPositionRange = 0.5f;
-            if (channelType == ChannelType.Multiplication)
-            {
-                count = count * magnitude;
-            }
-
-            else
-            {
-                count = count + magnitude;
-            }
             for (int i = 0; i < count; i++)
             {
                 Vector3 position = new Vector3(Random.Range(other.transform.position.x - randomPositionRange, other.transform.position.x + randomPositionRange),
